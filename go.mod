61c4750b63d8f8cb5106d5fbf88e2e17de1f6100
Yovi Prasetyo <yovilatte@gmail.com>
2020-12-21 08:52:57 +0700

    change idp lib flow

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/yoviprasetyo/idp
 
 go 1.15
+
+require (
+    github.com/gin-gonic/gin v1.6.3-0.20201025090830-16cd8cdd4ef9
+)
--- idp.go
+++ idp.go
@@ -1,22 +1,43 @@
 package idp
 
-import "encoding/json"
+import (
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+)
 
 // IDP struct.
 type IDP struct {
+	ID            string `json:"id"`
 	Sub           string `json:"sub"`
 	EmailVerified bool   `json:"email_verified"`
 	Name          string `json:"name"`
 	Username      string `json:"preferred_username"`
 	Email         string `json:"email"`
+	Active        bool   `json:"active"`
 }
 
 // Extract json string to IDP.
-func Extract(jsonString string) (IDP, error) {
+func Extract(jsonString []byte) (IDP, error) {
 	var idp IDP
-	err := json.Unmarshal([]byte(jsonString), &idp)
+	err := json.Unmarshal(jsonString, &idp)
 	if err != nil {
 		return idp, err
 	}
 	return idp, nil
+}
+
+// SetIDP method.
+func SetIDP(c *gin.Context, headerString string) (IDP, error) {
+	var (
+		encodedUserInfo = c.Request.Header.Get(headerString)
+	)
+
+	decodedUserInfo, err := base64.StdEncoding.DecodeString(encodedUserInfo)
+	if err != nil {
+		return IDP{}, err
+	}
+
+	return Extract(decodedUserInfo)
 }
